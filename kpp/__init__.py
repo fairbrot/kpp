3c45bf9a2fb0bcba9cc59012dce253aaf7968828
Jamie Fairbrother <jfairbrother87@hotmail.com>
2016-09-18 18:47:57 +0100

    Added more exports to package

--- kpp/__init__.py
+++ kpp/__init__.py
@@ -1,4 +1,4 @@
-from .separation import CliqueSeparator, Constraint, y_clique_constraint
-from .kpp import KPP
+from .separation import CliqueSeparator, Constraint, y_clique_constraint, YZCliqueSeparator, yz_clique_constraint
+from .kpp import KPP, KPPExtension
 from . import graph
 
