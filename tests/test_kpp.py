330462d29ed013a910852374fffab121ebc26ebe
Jamie Fairbrother <jfairbrother87@hotmail.com>
2016-10-05 17:18:45 +0100

    Rewrote tests to be more efficient

--- tests/test_kpp.py
+++ tests/test_kpp.py
@@ -6,30 +6,67 @@ from kpp import KPP, KPPExtension, YCliqueSeparator, ZCliqueSeparator, YZCliqueS
 seed(1)
 
 class TestKPP(unittest.TestCase):
-  def setUp(self):
-    self.G = ig.Graph.GRG(20, 0.5)
-    self.max_cliques = self.G.maximal_cliques()
-    print(self.max_cliques)
-    
-  def test_kpp(self):
-    plain_kpp = KPP(self.G, 3)
+  @classmethod
+  def setUpClass(cls):
+    super(TestKPP, cls).setUpClass()
+    print("Running TestKPP...")
+    cls.G = ig.Graph.GRG(30, 0.25)
+    cls.max_cliques = cls.G.maximal_cliques()
+    print(cls.max_cliques)
+    plain_kpp = KPP(cls.G, 3, verbosity=0)
     plain_kpp.solve()
-    plain_obj_val = plain_kpp.model.objVal
+    cls.obj_val = plain_kpp.model.objVal
 
-    cuts_kpp = KPP(self.G, 3)
+  def setUp(self):
+    pass
+
+  def test_cuts(self):
+    print("\ttest_cuts...")
+    cuts_kpp = KPP(self.G, 3, verbosity=0)
     cuts_kpp.add_separator(YCliqueSeparator(self.max_cliques, 4, 3))
     cuts_kpp.add_separator(YCliqueSeparator(self.max_cliques, 5, 3))
     cuts_kpp.cut()
     cuts_kpp.solve()
-    cuts_obj_val = cuts_kpp.model.objVal
-    self.assertAlmostEqual(plain_obj_val, cuts_obj_val)
+    obj_val = cuts_kpp.model.objVal
+    self.assertAlmostEqual(self.obj_val, obj_val)
 
-  def test_kpp_extension(self):
-    plain_kpp = KPPExtension(self.G)
+  def test_break_symmetry(self):
+    print("\ttest_break_symmetry...")
+    sym_kpp = KPP(self.G, 3, verbosity=0)
+    sym_kpp.break_symmetry()
+    sym_kpp.solve()
+    obj_val = sym_kpp.model.objVal
+    self.assertAlmostEqual(self.obj_val, obj_val)
+
+  def test_cuts_and_break_symmetry(self):
+    print("\ttest_cuts_and_break_symmetry...")
+    kpp = KPP(self.G, 3, verbosity=0)
+    kpp.add_separator(YCliqueSeparator(self.max_cliques, 4, 3))
+    kpp.add_separator(YCliqueSeparator(self.max_cliques, 5, 3))
+    kpp.cut()
+    kpp.break_symmetry()
+    kpp.solve()
+    obj_val = kpp.model.objVal
+    self.assertAlmostEqual(self.obj_val, obj_val)
+
+class TestKPPExtension(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    super(TestKPPExtension, cls).setUpClass()
+    print("Running TestKPPExtension...")
+    cls.G = ig.Graph.GRG(14, 0.6)
+    cls.max_cliques = cls.G.maximal_cliques()
+    print(cls.max_cliques)
+    plain_kpp = KPPExtension(cls.G, verbosity=0)
     plain_kpp.solve()
-    plain_obj_val = plain_kpp.model.objVal
+    cls.obj_val = plain_kpp.model.objVal
 
-    cuts_kpp = KPPExtension(self.G)
+  def setUp(self):
+    pass
+
+  def test_cuts(self):
+    print("\ttest_cuts...")
+    cuts_kpp = KPPExtension(self.G, verbosity=0)
     y_sep_alg_1 = YCliqueSeparator(self.max_cliques, 4, 3)
     y_sep_alg_2 = YCliqueSeparator(self.max_cliques, 5, 3)
     yz_sep_alg = YZCliqueSeparator(self.max_cliques, 7)
@@ -44,6 +81,35 @@ class TestKPP(unittest.TestCase):
     cuts_kpp.cut()
     cuts_kpp.solve()
     cuts_obj_val = cuts_kpp.model.objVal
-    self.assertAlmostEqual(plain_obj_val, cuts_obj_val)
+    self.assertAlmostEqual(self.obj_val, cuts_obj_val)
+
+  def test_break_symmetry(self):
+    print("\ttest_break_symmetry...")
+    sym_kpp = KPPExtension(self.G, verbosity=0)
+    sym_kpp.break_symmetry()
+    sym_kpp.solve()
+    obj_val = sym_kpp.model.objVal
+    self.assertAlmostEqual(self.obj_val, obj_val)
+
+  def test_cuts_and_break_symmetry(self):
+    print("\ttest_cuts_and_break_symmetry...")
+    kpp = KPPExtension(self.G, verbosity=0)
+    y_sep_alg_1 = YCliqueSeparator(self.max_cliques, 4, 3)
+    y_sep_alg_2 = YCliqueSeparator(self.max_cliques, 5, 3)
+    yz_sep_alg = YZCliqueSeparator(self.max_cliques, 7)
+    z_sep_alg = ZCliqueSeparator(self.max_cliques, 8)
+    kpp.add_separator(y_sep_alg_1)
+    kpp.add_separator(y_sep_alg_2)
+    kpp.cut()
+    kpp.sep_algs=[]
+    kpp.add_z_variables()
+    kpp.add_separator(yz_sep_alg)
+    kpp.add_separator(z_sep_alg)
+    kpp.cut()
+
+    kpp.break_symmetry()
+    kpp.solve()
+    obj_val = kpp.model.objVal
+    self.assertAlmostEqual(self.obj_val, obj_val)
 
 unittest.main()
